cba4d9528ef079bdd962b54aeb7facc68c67d0e6
Anna Orlova <annaorlova@Anna.local>
2021-08-30 16:36:20 +0100

    registration

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header + ' bg-blue-900'}>
         <img src='https://git-scm.com/images/logos/1color-darkbg@2x.png' />
+
+        <div className={s.loginBlock }>
+            {props.isAuth ? props.login :
+            <NavLink to = {'/login'}>Login</NavLink>}
+            
+        </div>
     </header>
 }
 
