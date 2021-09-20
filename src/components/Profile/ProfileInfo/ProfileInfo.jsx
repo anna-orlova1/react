e4c18244bc74131183642d9c808a18cc3c8a5437
Anna Orlova <annaorlova@Anna.local>
2021-09-20 17:51:17 +0100

    addingStatus

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from '../../common/Preloader/Preloader';
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
 
-if (!props.profile) {
-    return <Preloader/>
-}
+    if (!props.profile) {
+        return <Preloader />
+    }
 
     return (
         <div>
-            <div>
+            {/* <div>
                 <img
                     className="w-full"
                     src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
-            </div>
+            </div> */}
             <div className={s.descriptionBlock}>
-                <img src={props.profile.photos.large} />
-                ava + description 
+                <img src={props.profile.photos.large} /> 
+                <ProfileStatus status={"hello my "}/>
             </div>
         </div>
     )
